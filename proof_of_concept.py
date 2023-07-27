439b750a28900559054131631936359be3c225ba
Wren McQueary <wren.mcqueary@gmail.com>
2023-07-26 21:56:29 -0400

    Wrote proof of concept and got analog input and digital output to be compatible!

---
+++ proof_of_concept.py
@@ -0,0 +1,37 @@
+"""Proof of concept
+"""
+
+
+from time import sleep
+
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+import RPi.GPIO as GPIO
+
+
+
+# Set up MCP3008 ADC
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.D5)
+mcp = MCP.MCP3008(spi, cs)
+channel = AnalogIn(mcp, MCP.P0)
+
+
+# Set up GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)
+
+while True:
+    voltage = channel.voltage
+    print(voltage)
+    if voltage > 1.5:
+        print("High")
+        GPIO.output(14, GPIO.HIGH)
+    else:
+        print("Low")
+        GPIO.output(14, GPIO.LOW)
+    sleep(1)
--- util.py
+++ util.py
@@ -2,29 +2,29 @@
 """
 
 
-import RPi.GPIO as GPIO
 from time import sleep
 
-#import busio
-#import digitalio
-#import board
-#import adafruit_mcp3xxx.mcp3008 as MCP
-#from adafruit_mcp3xxx.analog_in import AnalogIn
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+import RPi.GPIO as GPIO
 
 
 
 # Set up MCP3008 ADC
-# TODO: Alter our system for GPIO stuff so it isn't conflicting with this
-#spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-#cs = digitalio.DigitalInOut(board.D5)
-#mcp = MCP.MCP3008(spi, cs)
-#channel = AnalogIn(mcp, MCP.P0)
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.D5)
+mcp = MCP.MCP3008(spi, cs)
+channel = AnalogIn(mcp, MCP.P0)
 
 
 # Set up GPIO
-GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering.  The alternative is GPIO.setmode(GPIO.BCM), which uses "NAME" on the pinout diagram instead of "Pin#".
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin, and set initial value to low.
-GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)   # Set pin 10 to be an output pin, and set initial value to low.
+GPIO.setmode(GPIO.BCM)   # Use physical pin numbering.  The alternative is GPIO.setmode(GPIO.BCM), which uses "NAME" on the pinout diagram instead of "Pin#".
+GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin, and set initial value to low.
+GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)   # Set pin 10 to be an output pin, and set initial value to low.
 
 
 
@@ -40,7 +40,8 @@ def get_temperature() -> float:
 
 def push_button() -> None:
     """Actuate the motor to press and release the button on the
-    air conditioning unit"""
-    GPIO.output(8, GPIO.HIGH)
-    sleep(2)
-    GPIO.output(8, GPIO.LOW)
+    air conditioning unit
+    """
+    GPIO.output(14, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(14, GPIO.LOW)
